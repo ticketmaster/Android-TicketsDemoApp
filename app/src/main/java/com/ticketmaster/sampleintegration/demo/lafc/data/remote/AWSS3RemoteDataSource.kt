688d96a2c876f07112dc1f336b71f7930677a5fd
Luis Burgos <luis.burgos@ticketmaster.com>
2025-03-18 10:43:33 -0500

    - Updated amazon library from Java to Android SDK. - Updated logic to retrieve the Amazon file.

--- app/src/main/java/com/ticketmaster/sampleintegration/demo/lafc/data/remote/AWSS3RemoteDataSource.kt
+++ app/src/main/java/com/ticketmaster/sampleintegration/demo/lafc/data/remote/AWSS3RemoteDataSource.kt
@@ -1,56 +1,43 @@
 package com.ticketmaster.sampleintegration.demo.lafc.data.remote
 
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.regions.Region
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.model.GetObjectRequest
 import com.ticketmaster.sampleintegration.demo.lafc.TicketCSV
 import com.ticketmaster.sampleintegration.demo.lafc.data.AWSS3DataSource
-import com.ticketmaster.tickets.util.Log
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
-import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient
-import software.amazon.awssdk.regions.Region
-import software.amazon.awssdk.services.s3.S3Client
-import software.amazon.awssdk.services.s3.model.GetObjectRequest
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
 class AWSS3RemoteDataSource(
     clientId: String,
     secret: String,
-    private val client: S3Client = S3Client.builder()
-        .httpClient(UrlConnectionHttpClient.create())
-        .region(Region.US_WEST_2)
-        .credentialsProvider(
-            StaticCredentialsProvider.create(
-                AwsBasicCredentials.create(
-                    clientId,
-                    secret
-                )
-            )
-        )
-        .build(),
+    private val client:AmazonS3Client = AmazonS3Client(
+        BasicAWSCredentials(clientId, secret),
+        Region.getRegion(Regions.US_WEST_2)
+    ),
 ) : AWSS3DataSource {
+        override suspend fun getCSVFile(bucketName: String, fileName: String): List<TicketCSV> {
+            val s3Object = client.getObject(GetObjectRequest(bucketName, fileName))
+            val reader = BufferedReader(InputStreamReader(s3Object.objectContent))
+            val csvData = mutableListOf<TicketCSV>()
 
-    override suspend fun getCSVFile(bucketName: String, fileName: String): List<TicketCSV> {
-        val request =
-            GetObjectRequest.builder().bucket(bucketName).key(fileName).build()
-        val inputStream = client.getObject(request)
-        val reader = BufferedReader(InputStreamReader(inputStream))
-        val csvData = mutableListOf<TicketCSV>()
-
-        reader.useLines { lines ->
-            lines.drop(1).forEach { line ->
-                val columns = line.split(",")
-                if (columns.size >= 3) {
-                    val ticketCSV = TicketCSV(
-                        columns[0].trim(),
-                        columns[1].trim(),
-                        columns[2].trim(),
-                        columns[3].trim(),
-                        columns[4].trim()
-                    )
-                    csvData.add(ticketCSV)
+            reader.useLines { lines ->
+                lines.drop(1).forEach { line ->
+                    val columns = line.split(",")
+                    if (columns.size >= 3) {
+                        val ticketCSV = TicketCSV(
+                            columns[0].trim(),
+                            columns[1].trim(),
+                            columns[2].trim(),
+                            columns[3].trim(),
+                            columns[4].trim()
+                        )
+                        csvData.add(ticketCSV)
+                    }
                 }
             }
+            return csvData
         }
-        return csvData
-    }
 }
